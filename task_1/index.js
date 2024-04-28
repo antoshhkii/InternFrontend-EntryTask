8a8d78c5f64cbca861f283bf5b2374662ba2fcfe
Anton Greiver <97595279+antoshhkii@users.noreply.github.com>
2024-04-28 15:20:31 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,36 @@
 import {encoded, translations} from './data.js'
 
 console.log("Let's rock")
-console.log(encoded, translations)
-
-
-
-// console.log(decoded)
+export const decodeFields = (encoded, translations) => {
+    const decoded = encoded.map(item => {
+      const decodedItem = { ...item };
+      for (const key in decodedItem) {
+        if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+          const id = decodedItem[key];
+          if (id !== null && translations[id]) {
+            decodedItem[key] = translations[id];
+          }
+        }
+      }
+      return decodedItem;
+    });
+  
+    const uniqueIds = new Set();
+    encoded.forEach(item => {
+      for (const key in item) {
+        if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+          const id = item[key];
+          if (id !== null) {
+            uniqueIds.add(id);
+          }
+        }
+      }
+    });
+  
+    return { decoded, uniqueIds: [...uniqueIds] };
+  };
+  
+  const { decoded, uniqueIds } = decodeFields(encoded, translations);
+  console.log('Decoded:', decoded);
+  console.log('Unique Ids:', uniqueIds);
+  
